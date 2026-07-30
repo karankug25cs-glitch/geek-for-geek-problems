84351688038311ed30d208d13bd3fc3599990d0c
karank <karank.ug25.cs@nitp.ac.in>
2026-07-30 22:03:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,20 +1,18 @@
 class Solution {
     public int getSecondLargest(int[] arr) {
-       int max = Integer.MIN_VALUE;
+        int largest = -1;
+        int slargest = -1 ;
+      //  int cout =0 ;
        for(int i=0; i<arr.length; i++){
-           if(max < arr[i])
-                max = arr[i];
-                
-       }
-       int smax = -1 ;
-       int cout =0 ;
-       for(int j=0; j<arr.length; j++){
-           if(smax < arr[j] && arr[j] != max){
-               smax = arr[j];
-               cout++;
+           if(largest < arr[i]){
+               slargest = largest;
+               largest = arr[i];
            }
+           else if(largest > arr[i] && slargest < arr[i]){
+               slargest = arr[i];
+           }
+        
        }
-        if(cout == 0) return -1 ;
-        else return smax;
+        return slargest;
     }
 }
