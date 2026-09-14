c3aeb84f025434c83513e997e682def56815e9dd
karan kumar <karank.ug25.cs@nitp.ac.in>
2026-09-14 20:14:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Array/sum-of-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int arraySum(int arr[]) {
+        int sum = 0;
+        for(int i=0; i<arr.length ; i++){
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+}
